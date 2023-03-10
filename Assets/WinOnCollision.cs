4643070d26bcadd5c550a7956982fa1a3e7074d7
MitchellGascoigne <107447804+MitchellGascoigne@users.noreply.github.com>
2023-03-10 14:18:12 +1100

    v5

--- Assets/WinOnCollision.cs
+++ Assets/WinOnCollision.cs
@@ -5,10 +5,21 @@ public class WinOnCollision : MonoBehaviour
     // Set this variable to the game object you want to check for collision with
     public GameObject winObject;
 
+    private bool hasWon = false;
+
     private void OnCollisionEnter(Collision collision)
     {
         // Check if the collision is with the win object and the player has the "Player" tag
         if (collision.gameObject == winObject && collision.collider.CompareTag("Player"))
+        {
+            hasWon = true;
+            Destroy(winObject);
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (hasWon)
         {
             // Display "You Win" on the game screen
             GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 25, 100, 50), "You Win!");
